68cdc1cee571938e87dc67f2d3e305ab77a1a9f3
jodyu <jody_unterschutz@tcenergy.com>
2019-12-13 12:30:10 -0700

    m

---
+++ TeamGame.Domain/Playoffs/Rules/PlayoffSeriesRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeamGame.Domain.Playoffs.Rules
+{
+    public class PlayoffSeriesRule
+    {
+        public string RuleName { get; set; }
+        public string SeriesName { get; set; }
+        public string HomeTeamFrom { get; set; }        
+
+    }
+}
