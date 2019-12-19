bf566ecc5642e9f6a6338ea06764639bbc9c52f7
jodyu <jody_unterschutz@tcenergy.com>
2019-12-19 10:07:47 -0700

    m

--- TeamGame.Domain/Playoffs/Playoff.cs
+++ TeamGame.Domain/Playoffs/Playoff.cs
@@ -8,15 +8,15 @@ namespace TeamGame.Domain.Playoffs
     public class Playoff : ICompetition, IDataObject
     {
         public long Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public IList<ICompetitionTeam> Teams { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int Number { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int Year { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public int StartingDay { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public bool Started { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IList<ICompetitionTeam> Teams { get; set; }
+        public int Number { get; set; }
+        public int Year { get; set; }
+        public int StartingDay { get; set; }
+        public string Name { get; set; }
+        public bool Started { get; set; }
         public bool Complete { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public IGameCreator GameCreator { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }        
-
+        public IGameCreator GameCreator { get; set; }
+        public IList<PlayoffSeries> Series { get; set; }
         public void AddTeam(ICompetitionTeam team)
         {
             throw new NotImplementedException();
